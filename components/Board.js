437923464537634fd4a6797bbbcb9363e5ea2570
SHARUN266 <sharunattari266@gmail.com>
2022-12-29 00:08:28 +0530

    SudokuGenerator

---
+++ components/Board.js
@@ -0,0 +1,22 @@
+import { getSudoku } from "sudoku-gen";
+export default function Board(){
+    let sudoku = getSudoku("easy");
+  sudoku = sudoku.puzzle;
+  console.log(sudoku);
+  let count = 0;
+  let temp = [];
+  for (var i = 0; i < 9; i++) {
+    let str = sudoku.substring(count, count + 9);
+    str = str
+      .trim()
+      .split("")
+      .map((e) => {
+        return isNaN(e) ? 0 : +e;
+      });
+
+    temp.push(str);
+    count = count + 9;
+  }
+  
+  return temp
+}
--- pages/SudokuGenerator.js
+++ pages/SudokuGenerator.js
@@ -1,18 +1,9 @@
-import React from 'react'
+import React from "react";
+import Board from "../components/Board";
 
-import { getSudoku } from 'sudoku-gen';
+
 export default function SudokuGenerator() {
-    let sudoku = getSudoku('easy');
-    sudoku=sudoku.puzzle.trim().split("");
-    console.log(sudoku)
-    let count=0
-    for(var i=0;i<sudoku.length;i=i+9){
-        let arr=[]
-        // console.log(sudoku[i])
-       // console.log(arr)
-    }
-    
-  return (
-    <div>SudokuGenerator</div>
-  )
+  
+
+  return <div>{Board()}</div>;
 }
