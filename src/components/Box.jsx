6ad19b46a4e8866659869fb7f74043d157bc09d3
SHARUN266 <sharunattari266@gmail.com>
2023-01-02 18:19:11 +0530

    Hello

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Box, Flex, Input, Stack } from "@chakra-ui/react";
-export default function SingleBox({ value }) {
-  return (
-    <Input
+// import React from "react";
+// import { Box, Flex, Input, Stack } from "@chakra-ui/react";
+// export default function SingleBox({ value }) {
+//   return (
+//     <Input
     
-      value={value}
-      display={"flex"}
+//       value={value}
+//       display={"flex"}
       
-      borderRadius={"sm"}
-      shadow={"lg"}
-      h="20px"
-      p="16px"
-      alignItems={"center"}
-      border={"1px solid lightGray"}
-    textAlign={"center"}
-    />
-  );
-}
+//       borderRadius={"sm"}
+//       shadow={"lg"}
+//       h="20px"
+//       p="16px"
+//       alignItems={"center"}
+//       border={"1px solid lightGray"}
+//     textAlign={"center"}
+//     />
+//   );
+// }
